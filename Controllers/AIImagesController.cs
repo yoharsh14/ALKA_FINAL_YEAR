71f37b06775a86ea16827eba1a35af7de933c28a
yoharsh14 <86423823+yoharsh14@users.noreply.github.com>
2024-10-21 16:39:24 +0530

    changes

--- Controllers/AIImagesController.cs
+++ Controllers/AIImagesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,10 +14,14 @@ namespace WDP2024Assignment2.Controllers
     public class AIImagesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger<AIImagesController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public AIImagesController(ApplicationDbContext context)
+        public AIImagesController(ApplicationDbContext context, ILogger<AIImagesController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         // GET: AIImages
@@ -46,7 +51,9 @@ namespace WDP2024Assignment2.Controllers
         // GET: AIImages/Create
         public IActionResult Create()
         {
-            return View();
+            AIImage aIImage = new();
+            aIImage.UploadDate = DateTime.Today;
+            return View(aIImage);
         }
 
         // POST: AIImages/Create
@@ -54,13 +61,27 @@ namespace WDP2024Assignment2.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Prompt,ImageGenerator,UploadDate,Filename,Like,canIncreaseLike")] AIImage aIImage)
+        public async Task<IActionResult> Create([Bind("Id,Prompt,ImageGenerator,UploadDate,Filename,Like,canIncreaseLike")] AIImage aIImage, IFormFile Filename)
         {
             if (ModelState.IsValid)
             {
+                if (Filename != null)
+                {
+                    string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + Filename.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        Filename.CopyTo(fileStream);
+                    }
+
+                    aIImage.Filename = "/images/" + uniqueFileName;
+                }
+
                 _context.Add(aIImage);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index", "Home");
             }
             return View(aIImage);
         }
@@ -86,34 +107,48 @@ namespace WDP2024Assignment2.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Prompt,ImageGenerator,UploadDate,Filename,Like,canIncreaseLike")] AIImage aIImage)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Prompt,ImageGenerator,UploadDate,Filename,Like,canIncreaseLike")] AIImage aIImage, IFormFile Filename)
         {
             if (id != aIImage.Id)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            AIImage data = new();
+            data.Id = aIImage.Id;
+            data.Prompt = aIImage.Prompt;
+            data.ImageGenerator = aIImage.ImageGenerator;
+            data.Like = aIImage.Like;
+            if (Filename != null)
             {
-                try
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + Filename.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
                 {
-                    _context.Update(aIImage);
-                    await _context.SaveChangesAsync();
+                    Filename.CopyTo(fileStream);
                 }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AIImageExists(aIImage.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+
+                data.Filename = "/images/" + uniqueFileName;
             }
-            return View(aIImage);
+            try
+            {
+                _context.Update(data);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AIImageExists(aIImage.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: AIImages/Delete/5
@@ -152,6 +187,26 @@ namespace WDP2024Assignment2.Controllers
         private bool AIImageExists(int id)
         {
             return _context.AIImage.Any(e => e.Id == id);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Like(int Id)
+        {
+            try
+            {
+                var data = await _context.AIImage.Where(x => x.Id == Id).FirstOrDefaultAsync();
+                data.Like = data.Like + 1;
+                _context.AIImage.Update(data);
+                await _context.SaveChangesAsync();
+                ViewData["SuccessMessage"] = "GenAI Liked Succesfully.";
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                ViewData["ErrorMessage"] = "Error Ocurred.";
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WDP2024Assignment2.Data;
 using WDP2024Assignment2.Models;
 
 namespace WDP2024Assignment2.Controllers;
@@ -7,15 +9,17 @@ namespace WDP2024Assignment2.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        return View(await _context.AIImage.OrderByDescending(item => item.Like).ToListAsync());
     }
 
     public IActionResult Contact()
